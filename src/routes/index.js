1e588ec7bda9906a57f325935761c927ca29edca
Rekhta Menahil <rekhta674@gmail.com>
2025-08-22 14:43:26 +0500

    setup production routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const workers = require("./workers.routes");
 const auth = require("./auth.routes");
+const production = require("./production.routes");
 
 const router = Router();
 router.use("/workers", workers);
 router.use("/auth", auth);
+router.use("/production", production);
 module.exports = router;
