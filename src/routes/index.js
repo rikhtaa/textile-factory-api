9c99a464248cab86351db20c52306cf26401e4f9
Rekhta Menahil <rekhta674@gmail.com>
2025-08-22 16:23:58 +0500

    add loom routes setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const workers = require("./workers.routes");
 const auth = require("./auth.routes");
+const looms = require("./looms.routes");
 
 const router = Router();
 router.use("/workers", workers);
 router.use("/auth", auth);
+router.use("/looms", looms);
 module.exports = router;
