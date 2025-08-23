feec7200cb2e5dd3bcd4d7e01ad94caf274b9306
Rekhta Menahil <rekhta674@gmail.com>
2025-08-23 12:33:45 +0500

    fix index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,13 @@
 const { Router } = require("express");
 const workers = require("./workers.routes");
 const auth = require("./auth.routes");
-<<<<<<< HEAD
 const looms = require("./looms.routes");
-=======
 const production = require("./production.routes");
->>>>>>> cdcf3ff39f366321da7b6e0ce5d21966f09442ad
 
 const router = Router();
 router.use("/workers", workers);
 router.use("/auth", auth);
-<<<<<<< HEAD
 router.use("/looms", looms);
-=======
 router.use("/production", production);
->>>>>>> cdcf3ff39f366321da7b6e0ce5d21966f09442ad
+
 module.exports = router;
