91bd85212e09a2590d69f52a5f371f9fc4726775
Rekhta Menahil <rekhta674@gmail.com>
2025-09-01 13:15:00 +0500

    setup morgan

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ const routes = require("./routes");
 const app = express();
 const cors = require("cors");
 const { notFound, errorHandler } = require("./middleware/error");
-
+const morgan = require("morgan");
 
 app.use(express.json({ limit: "2mb" }));
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
 }));
+app.use(morgan("dev"));
 app.use("/api", routes);
 app.use(notFound);
 app.use(errorHandler);
