09c70d1363d97ebe8a19d17b8a6bcf2a9b56db0f
Rekhta Menahil <rekhta674@gmail.com>
2025-09-01 16:43:34 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const { connect } = require("./db/mongoose");
 const { env } = require("./config/env");
 (async () => {
 await connect();
-  app.get((req, res)=>{ res.send("hi")})
+  app.get("/",(req, res)=>{ res.send("hi")})
 app.listen(env.PORT, () => {
 console.log(`API listening on http://localhost:${env.PORT}`);
 });
