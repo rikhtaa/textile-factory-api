ebc951fed1ec53528829acdc7922dfa4580d8b42
Rekhta Menahil <rekhta674@gmail.com>
2025-09-01 13:00:51 +0500

    setup cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const routes = require("./routes");
 const app = express();
+const cors = require("cors");
+
 
 app.use(express.json({ limit: "2mb" }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true,
+}));
 app.use("/api", routes);
 
 module.exports = app;
